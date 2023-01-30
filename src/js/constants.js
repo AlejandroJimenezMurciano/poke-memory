4ea93b0dcb2a145da12b2be80d404cde90d6a6cb
AlejandroJimenezMurciano <alejimenezmurciano@gmail.com>
2023-01-30 18:49:59 +0100

    poke-memory

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,11 +1,13 @@
 const allCards = document.querySelectorAll('.card');
 
 const addShow = card => {
-    card.classList.add('card--show');
+  card.classList.add('card--show');
 };
-  
+
 const removeShow = card => {
-    card.classList.remove('card--show');
+  card.classList.remove('card--show');
 };
+
+/*Para que el programa chequee si la primera y la segunda carta escogidas son iguales o no*/
 
 export { allCards, addShow, removeShow };
--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,49 @@
 import '../scss/styles.scss';
 import { allCards, addShow, removeShow } from './constants.js';
 
+let firstPick;
+let secondPick;
+
 /*Para que al empezar el juego se giren todas las cartas y se vuelvan a girar solas una sola vez*/
 window.addEventListener('load', () => {
-    allCards.forEach(card => {
-      addShow(card);
-    });
-    const timeoutId = setTimeout(() => {
-      allCards.forEach(card => {
-        removeShow(card);
-      });
-      clearTimeout(timeoutId);
-    }, 3000);
+  allCards.forEach(card => {
+    addShow(card);
   });
-  
+  const timeoutId = setTimeout(() => {
+    allCards.forEach(card => {
+      removeShow(card);
+    });
+    clearTimeout(timeoutId);
+  }, 3000);
+});
+
+const checkCards = () => {
+  if (firstPick.dataset.number !== secondPick.dataset.number) {
+    removeShow(firstPick);
+    removeShow(secondPick);
+  } else {
+    firstPick.dataset.correct = true;
+    secondPick.dataset.correct = true;
+  }
+  firstPick = undefined;
+  secondPick = undefined;
+};
+
+const asignChoice = card => {
+  if (card.dataset.selected) return;
+  firstPick ? (secondPick = card) : (firstPick = card);
+  addShow(card);
+
+  card.addEventListener(
+    'transitioned',
+    () => {
+      if (firstPick && secondPick) checkCards();
+    },
+    { once: true }
+  );
+};
+
+document.addEventListener('click', ev => {
+  if (ev.target.classList.contains('card') && !ev.target.dataset.correct)
+    asignChoice(ev.target);
+});
