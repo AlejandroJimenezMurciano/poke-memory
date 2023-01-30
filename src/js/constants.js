4a2a24f7ec01573004f390f81d8824cd8d724b9c
AlejandroJimenezMurciano <alejimenezmurciano@gmail.com>
2023-01-30 15:26:30 +0100

    poke-memory

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,1 +1,11 @@
-const cardElement = document.getElementById('card');
+const allCards = document.querySelectorAll('.card');
+
+const addShow = card => {
+    card.classList.add('card--show');
+};
+  
+const removeShow = card => {
+    card.classList.remove('card--show');
+};
+
+export { allCards, addShow, removeShow };
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,17 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-//import { cardElement } from './constants';
+import { allCards, addShow, removeShow } from './constants.js';
+
+/*Para que al empezar el juego se giren todas las cartas y se vuelvan a girar solas una sola vez*/
+window.addEventListener('load', () => {
+    allCards.forEach(card => {
+      addShow(card);
+    });
+    const timeoutId = setTimeout(() => {
+      allCards.forEach(card => {
+        removeShow(card);
+      });
+      clearTimeout(timeoutId);
+    }, 3000);
+  });
+  
